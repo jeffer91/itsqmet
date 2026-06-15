1f1a88f6982c96c6ed43b102b259c662b29ae62f
Jefferson Villarreal <jeffersonvillarreal91@gmail.com>
2026-06-14 19:10:36 -0500

    Borrar todos los archivos del repositorio

--- Gestion/Excel/excel-bus.js
+++
@@ -1,9 +0,0 @@
-// =========================================================
-// Archivo: excel-bus.js
-// Ruta: /Gestion/Excel/excel-bus.js
-// Función: NOOP (antes manejaba Guardar separado; ahora el guardado está en Cargar)
-// =========================================================
-(function (window) {
-  "use strict";
-  window.ExcelBus = { boot() {} };
-})(window);
--- historico/txt_convertidos/historico-utils.js.txt
+++
@@ -1,8 +0,0 @@
-const HistoricoUtils = {
-  esc(str) {
-    return String(str ?? "")
-      .replaceAll("&", "&amp;")
-      .replaceAll("<", "&lt;")
-      .replaceAll(">", "&gt;");
-  }
-};
